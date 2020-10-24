6005e05be0c088c6e3c48a91dcdbf13252659572
Anastasia.Wang <gaintic926@gmail.com>
2020-10-24 22:31:20 +0800

    modify: utils class

--- Android/Laptop4you/app/src/main/java/com/laptop4you/utils/DatabaseOpenHelper.java
+++ Android/Laptop4you/app/src/main/java/com/laptop4you/utils/DatabaseOpenHelper.java
@@ -5,12 +5,6 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 public class DatabaseOpenHelper extends SQLiteOpenHelper {
     public static final String DB_NAME = "LaptopDB.db";
     public static final String DB_SUB_PATH = "/databases/" + DB_NAME;
@@ -22,7 +16,6 @@ public class DatabaseOpenHelper extends SQLiteOpenHelper {
         super(context, DB_NAME, null, 1);
         APP_DATA_PATH = context.getApplicationInfo().dataDir;
         this.context = context;
-        copyDBFile();
     }
 
     public boolean openDataBase() throws SQLException {
@@ -44,29 +37,5 @@ public class DatabaseOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-    }
-
-    private void copyDBFile(){
-        String appDataPath = context.getApplicationInfo().dataDir;
-        File dbFolder = new File(appDataPath + "/databases");//Make sure the /databases folder exists
-        dbFolder.mkdir();//This can be called multiple times.
-
-        File dbFilePath = new File(appDataPath + "/databases/LaptopDB.db");
-
-        try {
-            InputStream inputStream = context.getAssets().open("LaptopDB.db");
-            OutputStream outputStream = new FileOutputStream(dbFilePath);
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = inputStream.read(buffer))>0)
-            {
-                outputStream.write(buffer, 0, length);
-            }
-            outputStream.flush();
-            outputStream.close();
-            inputStream.close();
-        } catch (IOException e){
-            //handle
-        }
     }
 }
--- Android/Laptop4you/app/src/main/java/com/laptop4you/utils/Laptop.java
+++ Android/Laptop4you/app/src/main/java/com/laptop4you/utils/Laptop.java
@@ -1,9 +1,7 @@
 package com.laptop4you.utils;
-import android.database.Cursor;
-import android.util.Log;
 
 public class Laptop {
-    private String uuid;
+    public String uuid;
     public String name;
     public String price;
     public String processor;
@@ -11,35 +9,15 @@ public class Laptop {
     public String hard_drive;
     public String graphics;
     public String img;
-    private Database database;
 
-
-    public Laptop(String uuid, Database db){
-        this.uuid=uuid;
-        this.database=db;
-        init();
-    }
-
-    private void init(){
-        database.open();
-        Cursor cursor = database.exeSql("SElECT * FROM LAPTOPS WHERE UUID='"+this.uuid+"';");
-        if (cursor.moveToFirst()){
-            do{
-                this.name = cursor.getString(cursor.getColumnIndex("name"));
-                this.price = cursor.getString(cursor.getColumnIndex("price"));
-                this.processor = cursor.getString(cursor.getColumnIndex("processor"));
-                this.memory=cursor.getInt(cursor.getColumnIndex("memory"));
-                this.hard_drive=cursor.getString(cursor.getColumnIndex("hard_drive"));
-                this.graphics=cursor.getString(cursor.getColumnIndex("graphics"));
-                this.img=cursor.getString(cursor.getColumnIndex("img"));
-            }while (cursor.moveToNext());
-        }
-        cursor.close();
-        database.close();
-        Log.d("laptop",name+price+processor+memory+hard_drive+graphics+img);
-    }
-
-    public String getUuid() {
-        return uuid;
+    public  Laptop(String uuid,String name,String price,String processor,int memory,String hard_drive,String graphics,String img){
+        this.uuid = uuid;
+        this.name = name;
+        this.price = price;
+        this.processor = processor;
+        this.memory=memory;
+        this.hard_drive=hard_drive;
+        this.graphics=graphics;
+        this.img=img;
     }
 }

066174c46dab25fc7982b50f2604a9e47a5c417e
lijieyu <lijieyu125@gmail.com>
2020-10-24 22:36:44 +0800

    modify: add Intent and page change

--- Android/Laptop4you/app/src/main/java/com/laptop4you/QueryGameActivity.java
+++ Android/Laptop4you/app/src/main/java/com/laptop4you/QueryGameActivity.java
@@ -1,14 +1,52 @@
 package com.laptop4you;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QueryGameActivity extends AppCompatActivity {
+
+    private Button mBtnNext;
+    private CheckBox mCBmoba, mCBmmo, mCBsmall;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query_game);
+
+        mCBmoba = findViewById(R.id.game_opt_1);
+        mCBmmo = findViewById(R.id.game_opt_2);
+        mCBsmall = findViewById(R.id.game_opt_3);
+        mBtnNext = findViewById(R.id.btn_game_next);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("MESSAGE");
+
+        mBtnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(message.contains("office")){
+                    Intent intent = new Intent(v.getContext(), QueryWorkActivity.class);
+                    intent.putExtra("MESSAGE",message);
+                    startActivity(intent);
+                }
+                else if(message.contains("home")){
+                    Intent intent = new Intent(v.getContext(), QueryHomeActivity.class);
+                    intent.putExtra("MESSAGE",message);
+                    startActivity(intent);
+                }
+                else{
+                    Intent intent = new Intent(v.getContext(), QueryPriceActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
 
     }
 }
